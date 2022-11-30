e791ec5f85a52c3ebdbcf018433078e18dc2ad05
Lee Jarvis <leejarvis@fastmail.com>
2022-11-30 17:50:08 +0000

    Add desktop about page

--- lib/jarvis_web/controllers/page_controller.ex
+++ lib/jarvis_web/controllers/page_controller.ex
@@ -2,8 +2,10 @@ defmodule JarvisWeb.PageController do
   use JarvisWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home)
+  end
+
+  def about(conn, _params) do
+    render(conn, :about)
   end
 end
