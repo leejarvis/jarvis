7fc6456387fb1beeeb40b72086be8ecc347a26f4
Lee Jarvis <leejarvis@fastmail.com>
2022-12-01 09:15:11 +0000

    Redirect gear/uses posts

--- lib/jarvis_web/controllers/post_controller.ex
+++ lib/jarvis_web/controllers/post_controller.ex
@@ -1,11 +1,20 @@
 defmodule JarvisWeb.PostController do
   use JarvisWeb, :controller
 
+  @redirects %{
+    "gear-2018" => "/uses",
+    "gear-2019" => "/uses",
+  }
+
   def index(conn, _params) do
     render(conn, :index, posts: Jarvis.Blog.posts())
   end
 
   def show(conn, %{"year" => year, "id" => id}) do
-    render(conn, :show, post: Jarvis.Blog.get_post_by_year_and_id!(year, id))
+    if (redirect_path = @redirects[id]) do
+      redirect(conn, to: redirect_path)
+    else
+      render(conn, :show, post: Jarvis.Blog.get_post_by_year_and_id!(year, id))
+    end
   end
 end
