5368ed47097d0cc060635ca6b41f76758661c086
Lee Jarvis <leejarvis@fastmail.com>
2022-12-01 12:39:46 +0000

    Homepage style tweaks

--- lib/jarvis_web/controllers/page_controller.ex
+++ lib/jarvis_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule JarvisWeb.PageController do
   use JarvisWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    render(conn, :home, recent_posts: Jarvis.Blog.recent_posts())
   end
 
   def about(conn, _params) do
